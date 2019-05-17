dd38792ef9bbfc1fafdd3d1f5f29e5462414fb06
buruk123 <rafal.wawiorka@gmail.com>
2019-05-17 15:46:20 +0200

    20.php

--- zaj10/20.php
+++ zaj10/20.php
@@ -1,7 +1,20 @@
 <?php
+try {
     $dsn = 'mysql:dbname=zwierzeta;host=127.0.0.1';
     $user = 'root';
     $password = '';
     $db = new PDO($dsn, $user, $password);
     $db->exec("update zwierzeta_domowe set nazwa='Reksio' where id=3");
+    $sql = 'select gatunek, nazwa from zwierzeta_domowe';
+    foreach($db->query($sql) as $row) {
+        print $row['gatunek'] . "\n";
+        print $row['nazwa'] . "\n";
+    }
+    
+
+}
+catch (PDOException $e) {
+    echo 'Connection failed';
+}
+    
 ?>
